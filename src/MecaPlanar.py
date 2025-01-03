6ba0da4427bc2b12fa23487f5db0c628fc8c50d8
gboucher <gabriel.boucher@mecademic.com>
2025-01-03 08:51:01 -0500

    Copied my library from the past project with the automatic movement to make life simpler.

--- src/MecaPlanar.py
+++ src/MecaPlanar.py
@@ -30,7 +30,7 @@ class MecaPlanar():
             return connection_state
 
     def initialize(self, timeout: float = 10.0):
-        if not sys.is_master:
+        if not sys.is_master():
             sys.gain_mastership()
         bot.activate_xbots()
         maxTime = time.time() + timeout
@@ -58,6 +58,13 @@ class MecaPlanar():
     def get_pmc_status(self) -> pmc_types.PmcStatus:
         return sys.get_pmc_status()
 
+    def get_xbot_ids(self) -> list[int]:
+        status = bot.get_all_xbot_info(0)
+        ids = []
+        for state in status:
+            ids.append(state.xbot_id)
+        return ids
+
     def get_num_xbots(self) -> int:
         return len(bot.get_all_xbot_info(0))
 
@@ -74,13 +81,16 @@ class MecaPlanar():
         for stat in status:
             pos[stat.xbot_id] = (stat.x_pos, stat.y_pos)
 
-    def send_single_linear_command(self, xbot_id: int, xpos: float, ypos: float, vel: float = 1.0, acc: float = 10.0):
+    def send_single_linear_command(self, xbot_id: int, xpos: float, ypos: float, vel: float = 1.0, acc: float = 10.0) -> None:
         bot.linear_motion_si(xbot_id, xpos, ypos, vel, acc)
 
-    def send_multi_linear_commands(self, moves: list[PlanarMotorMove]):
+    def send_multi_linear_commands(self, moves: list[PlanarMotorMove]) -> None:
         for move in moves:
             bot.linear_motion_si(move.bot_id, move.xpos,
                                  move.ypos, move.vel, move.acc, final_speed=move.end_speed)
+
+    def send_auto_move_command(self, num_bot: int, xbot_ids: list[int], x_pos: list[float], y_pos: list[float]) -> None:
+        bot.auto_driving_motion_si(num_bot, xbot_ids, x_pos, y_pos)
 
     def wait_move_done(self, bot_id: int, timeout: float = 10.0) -> pmc_types.XbotState:
         while bot.get_xbot_status(xbot_id=bot_id).xbot_state is not pmc_types.XbotState.XBOT_IDLE:
@@ -88,3 +98,32 @@ class MecaPlanar():
                 return pmc_types.XbotState.XBOT_OBSTACLE_DETECTED
             time.sleep(0.5)
         return pmc_types.XbotState.XBOT_IDLE
+
+    def define_stereotype(self,
+                          mover_type: pmc_types.XbotType,
+                          id: int,
+                          payload: float = 0,
+                          size_pos_x: float = 0,
+                          size_neg_x: float = 0,
+                          size_pos_y: float = 0,
+                          size_neg_y: float = 0,
+                          perf_level: int = 0,
+                          cg_x: float = 0,
+                          cg_y: float = 0,
+                          cg_z: float = 0,
+                          emerg_d_acc: float = 20) -> None:
+        bot.define_mover_stereotype(mover_type,
+                                    id,
+                                    payload,
+                                    size_pos_x,
+                                    size_neg_x,
+                                    size_pos_y,
+                                    size_neg_y,
+                                    perf_level,
+                                    cg_x,
+                                    cg_y,
+                                    cg_z,
+                                    emerg_d_acc)
+
+    def assign_stereotype(self, bot_id: int, ster_id: int) -> None:
+        bot.assign_mover_stereotype(bot_id, ster_id)
