daa816364e1133a6240e5ad69b15ee335f43bb1e
gboucher <gabriel.boucher@mecademic.com>
2025-01-03 10:12:07 -0500

    Did the structure for the program. It will be fairly simple and mostly synchronous since doing a full state machine is too complex and would take too much time.

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from MecaPlanar import MecaPlanar
+from mecademicpy.robot import Robot
+
+
+# Robot setup and activation
+
+
+# Planar setup and activation
+
+
+# Main loop
+while True:
+    pass
+    # Robots swap the parts
+
+    # Robot pipettes the well plate
+
+    # xbots for pipetting swap
+
+    # Robot pipettes the vial
+
+    # xbots swap from swap positions to pipette positions
