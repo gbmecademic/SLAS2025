7eccc628a9496f5cf116ff232281417d917d9526
gboucher <gabriel.boucher@mecademic.com>
2025-01-06 15:19:27 -0500

    Updated the whole code with the new types and some of the functions that changed. Need to test it with the hardware and figure out the stereotype.

--- src/MecaPlanar.py
+++ src/MecaPlanar.py
@@ -21,11 +21,11 @@ class MecaPlanar():
 
     def connect(self, auto_connect: bool = True, ip: str = '192.168.10.100') -> bool:
         if auto_connect:
-            connection_state = sys.auto_connect_to_pmc()
+            connection_state = sys.auto_search_and_connect_to_pmc()
             self.is_connected = connection_state
             return connection_state
         else:
-            connection_state = sys.connect_to_pmc(ip)
+            connection_state = sys.connect_to_specific_pmc(ip)
             self.is_connected = connection_state
             return connection_state
 
@@ -34,7 +34,7 @@ class MecaPlanar():
             sys.gain_mastership()
         bot.activate_xbots()
         maxTime = time.time() + timeout
-        while sys.get_pmc_status() is not pmc_types.PmcStatus.PMC_FULLCTRL:
+        while sys.get_pmc_status() is not pmc_types.PMCSTATUS.PMC_FULLCTRL:
             time.sleep(0.5)
             if time.time() > maxTime:
                 raise TimeoutError("PMC Activation timeout")
@@ -42,7 +42,7 @@ class MecaPlanar():
     def activate_bots(self, timeout: float = 10.0):
         bot.activate_xbots()
         maxTime = time.time() + timeout
-        while sys.get_pmc_status() is not pmc_types.PmcStatus.PMC_FULLCTRL:
+        while sys.get_pmc_status() is not pmc_types.PMCSTATUS.PMC_FULLCTRL:
             time.sleep(0.5)
             if time.time() > maxTime:
                 raise TimeoutError("PMC Activation timeout")
@@ -50,57 +50,61 @@ class MecaPlanar():
     def deactivate_bots(self, timeout: float = 10.0):
         bot.deactivate_xbots()
         maxTime = time.time() + timeout
-        while sys.get_pmc_status() is not pmc_types.PmcStatus.PMC_INACTIVE:
+        while sys.get_pmc_status() is not pmc_types.PMCSTATUS.PMC_INACTIVE:
             time.sleep(0.5)
             if time.time() > maxTime:
                 raise TimeoutError("PMC Deactivation timeout")
 
-    def get_pmc_status(self) -> pmc_types.PmcStatus:
+    def get_pmc_status(self) -> pmc_types.PMCSTATUS:
         return sys.get_pmc_status()
 
     def get_xbot_ids(self) -> list[int]:
-        status = bot.get_all_xbot_info(0)
+        status = bot.get_all_xbot_info(
+            pmc_types.ALLXBOTSFEEDBACKOPTION(0))
         ids = []
         for state in status:
             ids.append(state.xbot_id)
         return ids
 
     def get_num_xbots(self) -> int:
-        return len(bot.get_all_xbot_info(0))
+        return len(bot.get_all_xbot_info(pmc_types.ALLXBOTSFEEDBACKOPTION(0)))
 
     def get_xbots_state(self) -> dict:
-        status = bot.get_all_xbot_info(0)
+        status = bot.get_all_xbot_info(
+            pmc_types.ALLXBOTSFEEDBACKOPTION(0))
         states = {}
         for stat in status:
             states[stat.xbot_id] = stat.xbot_state
         return states
 
     def get_xbots_pos(self) -> dict:
-        status = bot.get_all_xbot_info(0)
+        status = bot.get_all_xbot_info(
+            pmc_types.ALLXBOTSFEEDBACKOPTION(0))
         pos = {}
         for stat in status:
             pos[stat.xbot_id] = (stat.x_pos, stat.y_pos)
 
     def send_single_linear_command(self, xbot_id: int, xpos: float, ypos: float, vel: float = 1.0, acc: float = 10.0) -> None:
-        bot.linear_motion_si(1, xbot_id, xpos, ypos, vel, acc)
+        bot.linear_motion_si(1, xbot_id, pmc_types.POSITIONMODE(0),
+                             pmc_types.LINEARPATHTYPE(0), xpos, ypos, 0.0, vel, acc)
 
     def send_multi_linear_commands(self, moves: list[PlanarMotorMove]) -> None:
         for move in moves:
-            bot.linear_motion_si(1, move.bot_id, move.xpos,
-                                 move.ypos, move.vel, move.acc, final_speed=move.end_speed)
+            bot.linear_motion_si(1, move.bot_id, pmc_types.POSITIONMODE(0), pmc_types.LINEARPATHTYPE(0), move.xpos,
+                                 move.ypos, move.end_speed, move.vel, move.acc)
 
     def send_auto_move_command(self, num_bot: int, xbot_ids: list[int], x_pos: list[float], y_pos: list[float]) -> None:
         bot.auto_driving_motion_si(num_bot, xbot_ids, x_pos, y_pos)
 
-    def wait_move_done(self, bot_id: int, timeout: float = 10.0) -> pmc_types.XbotState:
-        while bot.get_xbot_status(xbot_id=bot_id).xbot_state is not pmc_types.XbotState.XBOT_IDLE:
-            if bot.get_xbot_status(xbot_id=bot_id).xbot_state == pmc_types.XbotState.XBOT_OBSTACLE_DETECTED:
-                return pmc_types.XbotState.XBOT_OBSTACLE_DETECTED
+    def wait_move_done(self, bot_id: int, timeout: float = 10.0) -> pmc_types.XBOTSTATE:
+        while bot.get_xbot_status(xbot_id=bot_id).xbot_state is not pmc_types.XBOTSTATE.XBOT_IDLE:
+            if bot.get_xbot_status(xbot_id=bot_id).xbot_state == pmc_types.XBOTSTATE.XBOT_OBSTACLE_DETECTED:
+                return pmc_types.XBOTSTATE.XBOT_OBSTACLE_DETECTED
             time.sleep(0.5)
-        return pmc_types.XbotState.XBOT_IDLE
+        return pmc_types.XBOTSTATE.XBOT_IDLE
 
     def define_stereotype(self,
-                          mover_type: pmc_types.XbotType,
+                          mover_type: pmc_types.XBOTTYPE,
                           id: int,
                           payload: float = 0,
                           size_pos_x: float = 0,
@@ -126,7 +130,7 @@ class MecaPlanar():
                                     emerg_d_acc)
 
     def assign_stereotype(self, bot_id: int, ster_id: int) -> None:
-        bot.assign_mover_stereotype(bot_id, ster_id)
+        bot.assign_stereotype_to_mover(bot_id, ster_id,)
 
     def start_macro(self, macro_id: int, xbot_id) -> None:
         bot.run_motion_macro(1, macro_id, xbot_id)
