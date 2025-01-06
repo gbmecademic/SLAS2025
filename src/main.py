f8f82c94b650aec51939186911917b7d805e9093
gboucher <gabriel.boucher@mecademic.com>
2025-01-06 12:05:23 -0500

    Setup for the MEca500s is done, forgot to stage the changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,39 @@
 from MecaPlanar import MecaPlanar
 from mecademicpy.robot import Robot
+from constants import *
 
 
 # Robot setup and activation
+pipette = Robot()
+vials = Robot()
+well = Robot()
+
+pipette.Connect()
+vials.Connect()
+well.Connect()
+
+pipette.ResetError()
+vials.ResetError()
+well.ResetError()
+
+pipette.ActivateAndHome()
+vials.ActivateAndHome()
+well.ActivateAndHome()
+pipette.WaitHomed()
+vials.WaitHomed()
+well.WaitHomed()
+
+pipette.ClearMotion()
+vials.ClearMotion()
+well.ClearMotion()
+
+pipette.ResumeMotion()
+vials.ResumeMotion()
+well.ResumeMotion()
+
+pipette.MoveJoints(*PIPETTE_SAFE_POS)
+vials.MoveJoints(*VIALS_SAFE_POS)
+well.MoveJoints(*WELL_SAFE_POS)
 
 
 # Planar setup and activation
