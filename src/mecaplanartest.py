7900e25e82be60df5947607af19796e64a7d3fcd
Gabriel Boucher <gabriel.boucher@mecademic.com>
2025-01-07 10:27:01 -0500

    Tested the modifications to the API and everything seems to be working, gonna start on the details of the main script and test the logic of the application.

--- src/mecaplanartest.py
+++ src/mecaplanartest.py
@@ -4,6 +4,12 @@ from utilities import PlanarStateSLAS, XbotType
 
 mp = MecaPlanar()
 mp.connect()
-mp.activate_bots()
-mp.start_macro(0, 0)
-mp.wait_move_done(0)
+mp.initialize()
+
+print(mp.get_num_xbots())
+print(mp.get_xbot_ids())
+mp.send_single_linear_command(1, 0.840, 0.120)
+mp.wait_move_done(1)
+print("Duck")
+mp.send_single_linear_command(1, 0.12, 0.12)
+mp.wait_move_done(1)
